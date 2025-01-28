15a6a39e483a929c0ef43dff5b7546e0dffe6be2
박한진 <phj9020@hotmail.com>
2025-01-28 15:46:15 +0900

    AuthLayout

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-xl text-brand-100 font-bold">
-      <h1>share file</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">share file</h1>
     </div>
   );
 }
